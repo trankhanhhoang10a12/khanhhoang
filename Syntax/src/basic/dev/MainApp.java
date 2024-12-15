7ee8f62a380a6a9e0bee9f4d68135f252e7fcd25
trankhanhhoang10a12 <khanhhoangtran207@gmail.com>
2024-12-15 10:24:47 +0700

    Add files via upload

---
+++ Syntax/src/basic/dev/MainApp.java
@@ -0,0 +1,19 @@
+package basic.dev;
+
+import java.util.List;
+
+/*
+* Comment nhieu dong
+*/
+public class MainApp {
+	public static void main (String[]args) {
+		String firtName ;
+		List personList;
+		double PI = 3.14;
+	}
+	public void themsanpham() {
+}
+	public double tinhTienLuong() {
+	return 0;
+	}
+}
