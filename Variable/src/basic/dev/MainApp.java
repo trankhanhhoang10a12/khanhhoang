fe09df0ffb931a954599f0f5035771460b36389f
trankhanhhoang10a12 <khanhhoangtran207@gmail.com>
2024-12-15 15:58:16 +0700

    Add files via upload

--- Variable/src/basic/dev/MainApp.java
+++ Variable/src/basic/dev/MainApp.java
@@ -1,5 +1,24 @@
 package basic.dev;
-
+/**
+*
+* @author TranThanhBinh
+*/
 public class MainApp {
-
+	public static void main(String[] args) {
+		/**
+		* Khai bao bien
+		*/
+		String firstName = "khanh hoang";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		System.out.println("tran khanh hoang:");
+		String khanh;
+		System.out.println("khanh hoang" + " " + "tran");
+		System.out.println("5,0: ");
+		System.out.println("Passed: " + passed);
+		//Khai bao hang. Hang phai viet hoa
+		final float PI = 3.14f;	
+}
 }
