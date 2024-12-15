a95242774f8eee8fdd320910722b1c5dc20c38ad
trankhanhhoang10a12 <khanhhoangtran207@gmail.com>
2024-12-15 10:25:18 +0700

    Add files via upload

---
+++ HeloWold/src/basic/dev/MainApp.java
@@ -0,0 +1,11 @@
+package basic.dev;
+
+public class MainApp {
+public static void main(String[]args) {
+	//TODO Auto-generated method stub
+int n= 120;
+byte b= (byte) n;
+System.out.println(b);
+
+}
+}
